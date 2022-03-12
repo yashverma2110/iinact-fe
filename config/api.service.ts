16b42e4d8f42abd1347eaf376edb8b14d6fc1c7e
yash2110 <yash@quizizz.com>
2022-03-13 04:40:24 +0530

    feat: adding prod backend URL

--- config/api.service.ts
+++ config/api.service.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const LOCAL_BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://iinact-be.vercel.app";
 
 const API_SERVICE = axios.create({
-  baseURL: LOCAL_BASE_URL,
+  baseURL: process.env.NODE_ENV === "development" ? LOCAL_BASE_URL : BASE_URL,
 });
 
 export { API_SERVICE };
