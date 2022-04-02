8051de8d25108ed7277113418d0c770b86613a87
yash2110 <yash@quizizz.com>
2022-04-02 16:27:36 +0530

    feat: making app PWA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,21 @@
-import { Html, Main, NextScript, Head } from "next/document";
-import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-const MyDocument = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
 export default MyDocument;
