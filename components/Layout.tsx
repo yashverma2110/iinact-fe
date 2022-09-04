2e5658b4fab2fd1e429449a1aabbfa34575d8da6
yashverma2110 <yash.silver2110@gmail.com>
2022-09-05 00:31:07 +0530

    feat(modal): added support for modal and auth form context

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Routes from '../metadata/Route';
 import Tooltip from './Tooltip';
+import Modal from './Modal';
+import { FormHandler } from './FormHandler';
+import { LoginFormMetadata } from '../metadata/form/auth.metadata';
 
 interface LayoutProps {
   children: any;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const [isAuthModalShowing, setIsAuthModalShowing] = useState<boolean>(false);
+
+  const handleSignup = () => {};
+
   return (
     <div className="h-screen w-screen flex">
       <aside className="flex flex-col justify-center h-full sidebar rounded-r-xl">
@@ -29,6 +36,19 @@ const Layout = ({ children }: LayoutProps) => {
         <header className="px-4 py-2">Header</header>
         <main className="p-4">{children}</main>
       </div>
+      <Modal
+        title="Sign up"
+        subtitle="Join us to start the journey"
+        size="md"
+        isShowing={isAuthModalShowing}
+        setIsShowing={setIsAuthModalShowing}
+      >
+        <FormHandler
+          context={LoginFormMetadata}
+          buttonTitle="Sign Up"
+          onSubmit={(data) => console.log('form', data)}
+        />
+      </Modal>
     </div>
   );
 };
